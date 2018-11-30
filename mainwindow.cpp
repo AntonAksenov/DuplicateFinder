a26dcc9189542bd0c6dac26bfbfec22cd8518a9c
AntonAksenov <37124085+AntonAksenov@users.noreply.github.com>
2018-12-01 02:37:19 +0300

    Update mainwindow.cpp
    remove commented code

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -77,71 +77,7 @@ int main_window::countAllDirFiles(const QDir& dir, QProgressDialog& progress)
     }
     return ans;
 }
-/*
-void main_window::hashAllDirFiles(const QDir& dir, QProgressDialog& progress)
-{
-    qDebug(dir.absolutePath().toStdString().c_str());
 
-    for(QFileInfo file_info: dir.entryInfoList(QDir::Files)) {
-        QFile file(file_info.absoluteFilePath());
-        if (!file.open(QIODevice::ReadOnly)) {
-            qFatal(QString("File %1 can't be openned").arg(file_info.absoluteFilePath()).toStdString().c_str());
-        } else {
-            QCryptographicHash hash(QCryptographicHash::Sha256);
-                if (hash.addData(&file)) {
-                    progress.setValue(progress.value() + 1);
-
-                    QString fileHash = hash.result().append(QString::number(file_info.size()));
-
-                    // for checking non hash files comparing
-                    //fileHash = "0";
-
-                    auto it = hashMap.find(fileHash);
-                    if (it == hashMap.end()) {
-                        hashMap.insert(fileHash, QSet<QString>());
-                        it = hashMap.find(fileHash);
-                    }
-                    it->insert(file_info.absoluteFilePath());
-                    if (it->size() == 2) {
-                        equalFiles.append(it);
-                    }
-                }
-        }
-        if(progress.wasCanceled()) {
-            return;
-        }
-    }
-    for (QString dirName : dir.entryList(QDir::Dirs)) {
-        if (dirName != "." && dirName != "..") {
-            QDir curDir = dir;
-            curDir.cd(dirName);
-            hashAllDirFiles(curDir, progress);
-            if(progress.wasCanceled()) {
-                return;
-            }
-        }
-    }
-}
-
-bool compareFiles(QString fileName1, QString fileName2) {
-    QFile file1(fileName1);
-    QFile file2(fileName2);
-    if (!file1.open(QIODevice::ReadOnly)) {
-        qFatal(QString("File %1 can't be openned").arg(fileName1).toStdString().c_str());
-        return false;
-    } else if (!file2.open(QIODevice::ReadOnly)) {
-        qFatal(QString("File %1 can't be openned").arg(fileName2).toStdString().c_str());
-        return false;
-    } else {
-        while (!file1.atEnd() && !file2.atEnd()) {
-            if(file1.readLine() != file2.readLine()) {
-                return false;
-            }
-        }
-        return file1.atEnd() && file2.atEnd();
-    }
-}
-*/
 void main_window::scan_directory(QString const& dir)
 {
     ui->treeWidget->clear();
@@ -156,12 +92,9 @@ void main_window::scan_directory(QString const& dir)
     QProgressDialog progress("counting files...", "Abort", 0, 100, this);
     progress.show();
     int cnt = countAllDirFiles(d, progress);
-    //progress.setMaximum(cnt + 1);
     progress.setLabelText("reading files...");
     progress.setWindowModality(Qt::WindowModal);
     QCoreApplication::processEvents();
-
-    //hashAllDirFiles(d, progress);
 
     qDebug(QString(QString("%1 files")).arg(cnt).toStdString().c_str());
 
@@ -179,9 +112,6 @@ void main_window::scan_directory(QString const& dir)
                     hash.addData(&file);
 
                     QString fileHash = hash.result().append(QString::number(file_info.size()));
-
-                    // for checking non hash files comparing
-                    //fileHash = "0";
 
                     auto it = hashMap.find(fileHash);
                     if (it == hashMap.end()) {
@@ -204,42 +134,7 @@ void main_window::scan_directory(QString const& dir)
     }
 
     qDebug(QString(QString("%1 equal files sets")).arg(equalFiles.size()).toStdString().c_str());
-    /*
-    QVector<QSet<QString>> newEqualFiles; //here files are certanly equal in one set
-
-    for(auto set : equalFiles) {
-        QVector<QSet<QString>> newSets;
-        for(QString fileName : *set) {
-            if(fileName != *set->begin()) {
-                if(!compareFiles(fileName, *set->begin())) {
-                    bool unique = true;
-                    for (auto newSet : newSets) {
-                        if(compareFiles(fileName, *newSet.begin())) {
-                            newSet.insert(fileName);
-                            unique = false;
-                            break;
-                        }
-                        if (progress.wasCanceled()) { return; }
-                    }
-                    if(unique) {
-                        QSet<QString> newSet;
-                        newSet.insert(fileName);
-                        newSets.append(newSet);
-                    }
-                }
-            }
-        }
-        for (auto newSet : newSets) {
-            for (QString fileName : newSet) {
-                set->remove(fileName);
-            }
-            if(newSet.size() > 1) {
-                newEqualFiles.append(newSet);
-            }
-            if (progress.wasCanceled()) { return; }
-        }
-    }
-    */
+    
     i = 0;
     for (auto set : equalFiles) {
         if(set->size() > 1) {
@@ -258,23 +153,6 @@ void main_window::scan_directory(QString const& dir)
             ui->treeWidget->addTopLevelItem(groupItem);
         }
     }
-    /*
-    for (auto set : newEqualFiles) {
-        QTreeWidgetItem* groupItem = new QTreeWidgetItem(ui->treeWidget);
-        groupItem->setText(0, QString("group %1").arg(++i));
-        groupItem->setText(1, QString::number(QFileInfo(*set.begin()).size()));
-        for (auto fileName : set) {
-            QTreeWidgetItem* fileItem = new QTreeWidgetItem();
-            fileItem->setText(0, fileName);
-            groupItem->addChild(fileItem);
-            if(progress.wasCanceled()) {
-                //ui->treeWidget->clear();
-                return;
-            }
-        }
-        ui->treeWidget->addTopLevelItem(groupItem);
-    }
-    */
     if(ui->treeWidget->topLevelItemCount() == 0) {
         QTreeWidgetItem* item = new QTreeWidgetItem(ui->treeWidget);
         item->setText(0, "Nothing to show");
